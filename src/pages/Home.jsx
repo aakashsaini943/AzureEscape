2c36758dc7921b48b81123cd5ec9c09fbea1fb93
Aakash Saini <aakashsaini943@gmail.com>
2026-01-31 23:25:40 +0530

    update project: fix ui

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,8 +7,7 @@ import CTA from "../components/sections/CTA/CTA";
 import SavingsCompareCard from "../components/sections/SavingCard/SavingsCompareCard";
 import FlashSale from "../components/sections/FlashSale/FlashSale";
 import FlightPromoBanner from "../components/sections/FlihtDelasBanner/FlightPromoBanner";
-import Minioffers from "../components/sections/offer/Minioffers";
-
+import Minioffers from "../components/sections/offer/MiniOffers";
 
 const Home = () => {
   return (
